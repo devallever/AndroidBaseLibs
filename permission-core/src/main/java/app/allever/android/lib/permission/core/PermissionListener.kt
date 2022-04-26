482789dcd7a899d7f170068bec690a798621c9df
devallever <devallever@163.com>
2022-04-26 22:55:22 +0800

    优化权限，添加是否弹出默认跳转设置权限弹窗接口

--- permission-core/src/main/java/app/allever/android/lib/permission/core/PermissionListener.kt
+++ permission-core/src/main/java/app/allever/android/lib/permission/core/PermissionListener.kt
@@ -3,12 +3,31 @@ package app.allever.android.lib.permission.core
 import android.app.Dialog
 
 interface PermissionListener {
+    /**
+     * 全部同意
+     */
     fun onAllGranted()
 
     //    fun onGranted(grantedList: MutableList<String>) {}
+    /**
+     * 拒绝
+     */
     fun onDenied(deniedList: MutableList<String>) {}
+
+    /**
+     * 总是拒绝
+     */
     fun alwaysDenied(deniedList: MutableList<String>) {}
+
+    /**
+     * 自定义跳转弹窗，否则使用默认的
+     */
     fun getSettingDialog(): Dialog?{
         return null
     }
+
+    /**
+     * 默认总是拒绝后弹出设置权限，设置false不弹出
+     */
+    fun needShowJumpSettingDialog(): Boolean = true
 }
