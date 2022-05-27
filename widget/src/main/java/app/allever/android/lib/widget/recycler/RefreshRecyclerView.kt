e84b4dcebde10eb27d09da6854548f96fe4e46df
devallever <devallever@163.com>
2022-05-27 23:24:23 +0800

    RefreshRecyclerView添加预加载功能

--- widget/src/main/java/app/allever/android/lib/widget/recycler/RefreshRecyclerView.kt
+++ widget/src/main/java/app/allever/android/lib/widget/recycler/RefreshRecyclerView.kt
@@ -6,15 +6,21 @@ import android.view.LayoutInflater
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import app.allever.android.lib.core.ext.log
 import app.allever.android.lib.widget.R
 import com.chad.library.adapter.base.viewholder.BaseViewHolder
 import com.scwang.smart.refresh.layout.SmartRefreshLayout
 import com.scwang.smart.refresh.layout.api.RefreshFooter
 import com.scwang.smart.refresh.layout.api.RefreshHeader
 
+/**
+ * 下拉刷新/上拉加载/预加载的RecyclerView
+ */
 class RefreshRecyclerView<Item> @JvmOverloads constructor(
     context: Context, attrs: AttributeSet? = null
 ) : ConstraintLayout(context, attrs) {
+
+    private val TAG = RefreshRecyclerView::class.java.simpleName
 
     var recyclerView: RecyclerView? = null
     var refreshLayout: SmartRefreshLayout? = null
@@ -23,6 +29,8 @@ class RefreshRecyclerView<Item> @JvmOverloads constructor(
 
     private var mCurrentPage = 0
     private var mListener: Listener? = null
+    private var mPreLoadCount: Int = 5
+    private var mIsPreLoading = false
 
     init {
         LayoutInflater.from(context).inflate(R.layout.refresh_recycler_view, this)
@@ -46,14 +54,51 @@ class RefreshRecyclerView<Item> @JvmOverloads constructor(
             mCurrentPage = 0
             mListener?.loadData(mCurrentPage)
         }
+
+        recyclerView?.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                // 获取 LayoutManger
+                val layoutManager = recyclerView.layoutManager
+                // 如果 LayoutManager 是 LinearLayoutManager
+                if (layoutManager is LinearLayoutManager) {
+                    // 如果列表正在往上滚动，并且表项最后可见表项索引值 等于 预加载阈值
+                    if (dy > 0 && layoutManager.findLastVisibleItemPosition() == layoutManager.itemCount - 1 - mPreLoadCount) {
+                        //
+                        if (mIsPreLoading) {
+                            return
+                        }
+
+                        mCurrentPage++
+//                        toast("预加载")
+                        log("预加载第 $mCurrentPage 页")
+                        mListener?.loadData(mCurrentPage)
+                        mIsPreLoading = true
+                        postDelayed({
+                            mIsPreLoading = false
+                        }, 2000)
+                    }
+                }
+            }
+        })
     }
 
+    /***
+     * @param refreshRVAdapter
+     * @param header 刷新头
+     * @param footer 加载底部
+     * @param layoutManager
+     * @param emptyResId 空布局
+     * @param preLoadCount 预加载阈值
+     * @param listener 刷新/加载监听器
+     */
     fun setAdapter(
         refreshRVAdapter: RefreshRVAdapter<Item, BaseViewHolder>,
         header: RefreshHeader? = null,
         footer: RefreshFooter? = null,
         layoutManager: RecyclerView.LayoutManager? = null,
         emptyResId: Int = R.layout.rv_empty_view,
+        preLoadCount: Int = 5,
         listener: Listener? = null,
     ) {
         recyclerView?.layoutManager = layoutManager ?: LinearLayoutManager(context)
@@ -70,22 +115,38 @@ class RefreshRecyclerView<Item> @JvmOverloads constructor(
         this.mListener = listener
         this.mCurrentPage = 0
         this.mListener?.loadData(mCurrentPage)
+        this.mPreLoadCount = preLoadCount
     }
 
 
     interface Listener {
+        /**
+         * @param currentPage 加载第n页数据
+         */
         fun loadData(currentPage: Int) {}
     }
 
+    /**
+     * 加载更多调用该方法
+     * @param list 追加数据
+     */
     fun loadMoreData(list: MutableList<Item>) {
         refreshRVAdapter?.adapter?.data?.addAll(list)
         refreshRVAdapter?.adapter?.notifyDataSetChanged()
         refreshLayout?.finishLoadMore(true)
     }
 
+    /**
+     * 刷新/首次加载数据调用该方法
+     * @param list 数据源
+     */
     fun refreshData(list: MutableList<Item>) {
         refreshRVAdapter?.adapter?.setList(list)
         refreshLayout?.finishRefresh(true)
+    }
+
+    private fun log(msg: String) {
+        log(TAG, msg)
     }
 
 }
