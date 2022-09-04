417ca0a96159411bad599d705a83fb7121321c4d
devallever <devallever@163.com>
2022-09-04 21:26:15 +0800

    优化BaseViewModel

--- mvvm/src/main/java/app/allever/android/lib/mvvm/base/BaseViewModel.kt
+++ mvvm/src/main/java/app/allever/android/lib/mvvm/base/BaseViewModel.kt
@@ -1,13 +1,40 @@
 package app.allever.android.lib.mvvm.base
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.cancel
 
 abstract class BaseViewModel : ViewModel() {
+
+    private val mJob by lazy {
+        Job()
+    }
+
+    protected val mainScope by lazy {
+        CoroutineScope(Dispatchers.Main + mJob)
+    }
+
+    protected val defaultScope by lazy {
+        CoroutineScope(Dispatchers.Default + mJob)
+    }
+
+    protected val ioScope by lazy {
+        CoroutineScope(Dispatchers.IO + mJob)
+    }
+
     /***
      * Activity#onCreate()后调用
      * Fragment#onCreateView()后调用
      */
     abstract fun init()
+
+    override fun onCleared() {
+        super.onCleared()
+        mainScope.cancel()
+        defaultScope.cancel()
+        ioScope.cancel()
+        mJob.cancel()
+    }
 }
