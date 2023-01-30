fe87ecb891add8c34f763db4af59ae241a2b255a
devallever <devallever@163.com>
2023-01-30 22:11:28 +0800

    提交遗漏文件

---
+++ imageloader-glide/src/main/java/app/allever/android/lib/imageloader/glide/MyAppGlideModule.java
@@ -0,0 +1,8 @@
+package app.allever.android.lib.imageloader.glide;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+}
